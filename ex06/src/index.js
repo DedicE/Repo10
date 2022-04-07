79ccd60f9e48a7bc0cfb31126f09f3ab30d08183
Emir Dedic <dedic.emir17@gmail.com>
2022-04-07 16:44:05 +0200

    Fix

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,16 +1,14 @@
-// Create lion object here
 var lion = {
     name: "Simba",
-    legs: "4",
-    tails: "1"
+    legs: 4,
+    tails: 1
 }
-// End of lion object
 
-// Create a function below this line
-function myFunction(propName, propValue){
-    lion[propName] = propValue; 
+function myFunction(propName, propValue) {
+    lion[propName] = propValue;
     return lion;
 }
-// End of function
+
 console.log(myFunction("roar", "roar-roar"));
+
 module.exports = myFunction;
